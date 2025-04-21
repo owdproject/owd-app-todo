7c473e3d454777801ab2aab9a16c6e83dfb7959e
dxlliv <hello@dxlliv.dev>
2025-04-21 04:20:45 +0200

    Enable plugin parallel feature

--- runtime/plugin/index.ts
+++ runtime/plugin/index.ts
@@ -2,8 +2,11 @@ import {defineNuxtPlugin} from "nuxt/app"
 import {defineDesktopApp} from "@owdproject/core/runtime/utils/utilsDesktop"
 import configAppTodo from '../../owd.config'
 
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.hook('app:created', async () => {
-        await defineDesktopApp(configAppTodo)
-    })
+export default defineNuxtPlugin({
+    parallel: true,
+    async setup(nuxtApp) {
+        nuxtApp.hook('app:created', async () => {
+            await defineDesktopApp(configAppTodo)
+        })
+    }
 })
